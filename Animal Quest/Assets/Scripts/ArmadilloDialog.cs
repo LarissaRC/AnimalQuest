06f6ca7598b14ef939d01a3e4559b90f8c67d244
LarissaRC <larissa.carvalho0309@gmail.com>
2022-06-07 23:37:46 -0400

    Dialog box added

---
+++ Animal Quest/Assets/Scripts/ArmadilloDialog.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArmadilloDialog : MonoBehaviour
+{
+    public GameObject dialogObj;
+    public Text speechText;
+    public string text;
+    private string sentence;
+    public float typingSpeed;
+
+    public LayerMask playerLayer;
+    public float radious;
+    bool onRadious, started = false;
+
+    private void FixedUpdate()
+    {
+        Interact();
+    }
+
+    public void Interact()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, radious, playerLayer);
+
+        // Se colidir
+        if(hit != null)
+        {
+            onRadious = true;
+        }
+        else
+        {
+            onRadious = false;
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Z) && onRadious && !started)
+        {
+            started = true;
+            Speech(text);
+        }
+    }
+
+    public void Speech(string txt)
+    {
+        dialogObj.SetActive(true);
+        sentence = txt;
+        StartCoroutine(TypeSentence());
+    }
+
+    IEnumerator TypeSentence()
+    {
+        foreach(char letter in sentence.ToCharArray())
+        {
+            speechText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void CloseDialogBox()
+    {
+        dialogObj.SetActive(false);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, radious);
+    }
+}
